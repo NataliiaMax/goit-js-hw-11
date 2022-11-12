71ea05881e1b91a6dfcc3fa988698bf6a1d6f9d9
NataliiaMax <105719575+NataliiaMax@users.noreply.github.com>
2022-11-12 17:55:05 +0200

    update axios

--- src/index.js
+++ src/index.js
@@ -1,16 +1,111 @@
 import './css/styles.css';
+import axios from 'axios';
 import Notiflix from 'notiflix';
-import { fetchGallery } from '../src/pixabay';
+import PixabayApiService from '../src/pixabay';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// export { refs };
+// import { fetchGallery } from '../src/pixabay';
+import PixabayApiService from '../src/pixabay';
+
+const refs = {
+  form: document.querySelector('#search-form'),
+  input: document.querySelector('input'),
+  container: document.querySelector('.gallery'),
+  button: document.querySelector('.load-more'),
+};
+
+const newPixabayApiService = new PixabayApiService();
+
+console.log(newPixabayApiService);
+console.log(refs.input);
 
 
-const formRef = document.querySelector('#search-form');
+refs.form.addEventListener('submit', onSubmitForm);
+refs.button.addEventListener('click', onloadMore);
+
+function onSubmitForm(ev) {
+  ev.preventDefault();
 
 
-formRef.addEventListener('submit', onSubmitFormRef);
+  newPixabayApiService.query = ev.currentTarget.elements.searchQuery.value;
+  newPixabayApiService.resetPage();
 
-function onSubmitFormRef(ev) {
-    ev.preventDefault();
-    const data = ev.target.value.trim();
-    fetchGallery(data)
-    console.log(data);
+  if (newPixabayApiService.query === '') {
+    galleryRef.innerHTML = '';
+  }
+
+  newPixabayApiService.fetchGallery().then(hits => {
+    clearGallery();
+    appendImages(hits)
+  })
+
+     
+      // if (hits.length === 0) {
+      //   Notiflix.Notify.info(
+      //     '"Sorry, there are no images matching your search query. Please try again."'
+      //   );
+      // } else {
+      //   refs.container.insertAdjacentHTML('beforeend', showGallery(image));
+      // }
+
 }
+
+function showGallery(hits) {
+  console.log(hits);
+  const markup = hits.map(
+    ({
+      webformatURL,
+      largeImageURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloadstags,
+    }) => {
+      `<div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes${likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views${views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments${comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads${downloadstags}</b>
+          </p>
+        </div>
+      </div>`;
+    }
+  );
+  return markup;
+}
+
+function appendImages(hits) {
+  if (hits.length === 0) {
+        Notiflix.Notify.info(
+          '"Sorry, there are no images matching your search query. Please try again."'
+        );
+      } else {
+        refs.container.insertAdjacentHTML('beforeend', showGallery(hits));
+      }
+
+  // refs.container.insertAdjacentHTML('beforeend', showGallery(hits));
+}
+
+function onloadMore() {
+  newPixabayApiService.fetchGallery().then(appendImages);
+}
+
+function clearGallery() {
+  // if (data === '') {
+    
+  // }
+  refs.container.innerHTML = '';
+}
+
+
--- src/pixabay.js
+++ src/pixabay.js
@@ -1,14 +1,89 @@
-export { fetchGallery};
+// export { fetchGallery };
+import axios from 'axios';  
+// const URL = 'https://pixabay.com/api/';
+// const FIELDS =
+//   `?key=31223226-3ecae3b2f04fb3bb58e55d840&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`;
 
-const URL = 'https://pixabay.com/api/';
-const FIELDS = "?fields=key='31223226-3ecae3b2f04fb3bb58e55d840,q,image_type='photo',orientation='horizontal',safesearch=true";
+  
+// function fetchGallery() {
+//   fetch(`${URL}${FIELDS}`)
+//       .then(responce => {
+    //  if (!response.ok) {
+    //    throw new Error(response.status);
+    //  }
+    //  return response.json();
+//    })
+//       };
+import Notiflix from 'notiflix';
+// import { refs } from '../src/index';
+export default class PixabayApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.image = '';
+  }
+  async fetchGallery() {
+    // const URL = 'https://pixabay.com/api/';
+    // const FIELDS = `?key=31223226-3ecae3b2f04fb3bb58e55d840&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
+  const response = await axios.get(`https://pixabay.com/api/?key=31223226-3ecae3b2f04fb3bb58e55d840&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`)
+const data = response.data;
+    
+    this.image = data.hits;
+  // .then(response => {
+      //   if (!response.ok) {
+      //     Notiflix.Notify.failure(
+      //       'Sorry, there are no images matching your search query. Please try again.'
+      //     );
+      //     throw new Error(response.status);
+      //   }
+      //   return response.json();
+      // })
+      // .then(data => {
+        this.incrementPage();
+       
+        return this.image;
+        // return data.hits;
+      }
+  
 
-function fetchGallery() {
-    fetch = `${URL}${FIELDS}`
-    .then(responce => {
-     if (!response.ok) {
-       throw new Error(response.status);
-     }
-     return response.json();
-   })
-      };
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
+
+// export { fetchGallery };
+
+// async function fetchGallery() {
+//   const url = await axios.get(
+//     'https://pixabay.com/api/?key=31223226-3ecae3b2f04fb3bb58e55d840&q&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1'
+//   )
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+//       const dataArr = url.data
+//       console.log(response);
+//       page += 1;
+//     })
+
+// }
+
+
+
+
+
+
+
+
